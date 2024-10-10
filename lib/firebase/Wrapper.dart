0c663babd5b0eef17a1fefaf3fc7a33f3d2d9444
Ghozi-Waridi <ghoziwaridi@gmail.com>
2024-10-10 20:58:11 +0700

    add and modify folder firebase

---
+++ lib/firebase/Wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:project_prak_mobile/firebase/Authenticate.dart';
+
+class Wrapper extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
--- lib/firebase/auth_service.dart
+++ lib/firebase/auth_service.dart
@@ -1,16 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance; // memanggil class firebaseAuth untuk melakukan operation autentikasi di firebase
 
   Future singInAnm() async {
-    // future itu harus  menggunankan async
+    // print("User berhasul LOgin ");
+    // future itu harus  menggunankan async 
     try {
+
       UserCredential userCredential = await _auth.signInAnonymously();
-      User? user = userCredential.user;
+      User? user = userCredential.user; 
       return user;
     } catch (e) {
-      return null;
+      return "Eror => " + e.toString();
     }
   }
 }
